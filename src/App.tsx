5b8a47b4e580cc03303458393bb0c4a47cfe76f7
Erick Castillo <erick.ccastillo54@gmail.com>
2026-09-07 10:57:50 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ const App: React.FC = () => {
   return (
     <div className="app-root">
       <Header />
-    <main className="container">
+    <main>
     <Routes>
       <Route path="/" element={<Home />} />
 
