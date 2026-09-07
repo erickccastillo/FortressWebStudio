08a30446f8ff343250389267277900a088c6ef34
Erick Castillo <erick.ccastillo54@gmail.com>
2026-09-07 10:41:06 -0600

    Fix import path for Header component

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
-import Header from "../components/Header";
+import Header from "./components/Header";
 
 const App: React.FC = () => {
   return (
