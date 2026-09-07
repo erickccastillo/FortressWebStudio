8029f9e808c9d51d546faeb6f0fb6050be9f3b88
Erick Castillo <erick.ccastillo54@gmail.com>
2026-09-07 11:38:02 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import NotFound from './pages/NotFound';
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const App: React.FC = () => {
   return (
@@ -18,7 +19,7 @@ const App: React.FC = () => {
 
 
      </main>
-
+<Footer />
       </div>
 
   );
