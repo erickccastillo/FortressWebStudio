80a576c67767cf3d9133ed64e9b2c0c899c0e89a
Erick Castillo <erick.ccastillo54@gmail.com>
2026-09-07 10:55:59 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ const App: React.FC = () => {
   return (
     <div className="app-root">
       <Header />
-      <main className="container">
+    
     <Routes>
       <Route path="/" element={<Home />} />
 
@@ -18,7 +18,7 @@ const App: React.FC = () => {
 
      </main>
 
-    </div>
+
   );
 };
 
