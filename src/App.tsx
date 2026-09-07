d1a2d0e1c4a862e30d650b2b1d16173efe5fd687
Erick Castillo <erick.ccastillo54@gmail.com>
2026-09-07 10:39:44 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
+import Header from "../components/Header";
 
 const App: React.FC = () => {
   return (
