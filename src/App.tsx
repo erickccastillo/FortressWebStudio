e55a956c79b964af732bf4565183d2fffd73b800
Erick <erick.ccastillo54@gmail.com>
2026-09-08 18:37:28 -0600

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,22 +5,21 @@ import Login from './pages/Login';
 import NotFound from './pages/NotFound';
 import Header from "./components/Header";
 
-
 const App: React.FC = () => {
   return (
-    <div className="app-root">
+    // Agregamos flex y flex-col para que el Header y el Main se acomoden bien
+    <div className="flex flex-col min-h-screen w-full bg-[#050810]">
       <Header />
-    <main className="h-[calc(100vh-80px)] overflow-y-auto custom-scroll">
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
-
-
-     </main>
-      </div>
-
+      
+      {/* Quitamos la altura fija y el overflow interno */}
+      <main className="flex-grow w-full custom-scroll">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </main>
+    </div>
   );
 };
 
