a8e5190e29f517d20dbcbb084d89cd0b14803d9e
Erick Castillo <erick.ccastillo54@gmail.com>
2026-09-14 06:43:29 -0600

    Update Home.tsx

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,8 +1,6 @@
 import { useState, useEffect, useRef } from 'react';
 import { 
   MessageCircle, 
-  GraduationCap, 
-  Briefcase,
   Mail 
 } from 'lucide-react';
 import miFoto from '../images/photo.png';
